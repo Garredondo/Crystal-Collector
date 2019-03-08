09b168f42e1a3cdb3ae81a6c7d58f3d04853486a
George Arredondo <arredondo.george@gmail.com>
2019-03-07 19:22:46 -0600

    Rearragned the reset button, added background image, and styled the font.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,128 +1,134 @@
-$(document).ready(function(){
-//set my global variables
+$(document).ready(function () {
+    //set my global variables ====================
+    var compScore = (Math.floor(Math.random() * (120 - 19 + 1)) + 19);
+    var wins = 0;
+    var losses = 0;
+    var crystalOneValue = "";
+    var crystalTwoValue = "";
+    var crystalThreeValue = "";
+    var crystalFourValue = "";
+    var userScore = "";
+    var gameComplete = false;
 
-var compScore = (Math.floor(Math.random() * (120 - 19 + 1)) + 19);
-var userScore = "";
-var wins = 0;
-var losses = 0;
-var crystalOneValue = "";
-var crystalTwoValue = "";
-var crystalThreeValue = "";
-var crystalFourValue = "";
+    //display compScore============================
+    // function randomComputerNumber(){
+    //     randomComputerNumber = (Math.floor(Math.random() * (120 - 19 + 1)) + 19);
+    //     $("#comp-score").text(randomComputerNumber());
+    // };
 
-//display compScore
+    $("#comp-score").text(compScore);
 
-$("#comp-score").text(compScore);
+    $("#reset-button").click(function () {
+        reset();
+    });
 
-//Function so that on-click a random number (1-12) is generated if the value is 0, if it has a value greater than 0 then don't run the randomizer and just add the value to the userScore.
-$("#crystal-one").on("click", function(){
-    if (crystalOneValue === "") {
-        crystalOneValue = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
-        console.log(crystalOneValue);
-        userScore = parseInt(userScore + crystalOneValue);
-        console.log("first click " + userScore);
-        $("#user-score").text(userScore);
-        scoreCompare();
-    } else {
-        userScore = parseInt(userScore + crystalOneValue);
-        console.log("second click " + userScore);
-        $("#user-score").text(userScore);
-        scoreCompare();
-    }
-});
+    // $("#comp-score").text(compScore);
+    //============================
 
-$("#crystal-two").on("click", function(){
-    if (crystalTwoValue === "") {
-        crystalTwoValue = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
-        console.log(crystalTwoValue);
-        userScore = parseInt(userScore + crystalTwoValue);
-        console.log("first click " + userScore);
-        $("#user-score").text(userScore);
-        scoreCompare();
-    } else {
-        userScore = parseInt(userScore + crystalTwoValue);
-        console.log("second click " + userScore);
-        $("#user-score").text(userScore);
-        scoreCompare();
-    }
-});
 
-$("#crystal-three").on("click", function(){
-    if (crystalThreeValue === "") {
-        crystalThreeValue = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
-        console.log(crystalThreeValue);
-        userScore = parseInt(userScore + crystalThreeValue);
-        console.log("first click " + userScore);
-        $("#user-score").text(userScore);
-        scoreCompare();
-    } else {
-        userScore = parseInt(userScore + crystalThreeValue);
-        console.log("second click " + userScore);
-        $("#user-score").text(userScore);
-        scoreCompare();
-    }
-});
+    //Function so that on-click a random number (1-12) is generated if the value is 0, if it has a value greater than 0 then don't run the randomizer and just add the value to the userScore.
+    $("#crystal-one").on("click", function () {
+        if (gameComplete === false) {
+            if (crystalOneValue === "") {
+                crystalOneValue = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
+                console.log(crystalOneValue);
+                userScore = parseInt(userScore + crystalOneValue);
+                console.log("first click " + userScore);
+                $("#user-score").text(userScore);
+                scoreCompare();
+            } else {
+                userScore = parseInt(userScore + crystalOneValue);
+                console.log("second click " + userScore);
+                $("#user-score").text(userScore);
+                scoreCompare();
+            }
+        }
+    });
 
-$("#crystal-four").on("click", function(){
-    if (crystalFourValue === "") {
-        crystalFourValue = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
-        console.log(crystalFourValue);
-        userScore = parseInt(userScore + crystalFourValue);
-        console.log("first click " + userScore);
-        $("#user-score").text(userScore);
-        scoreCompare();
-    } else {
-        userScore = parseInt(userScore + crystalFourValue);
-        console.log("second click " + userScore);
-        $("#user-score").text(userScore);
-        scoreCompare();
-    }
-});
+    $("#crystal-two").on("click", function () {
+        if (gameComplete === false) {
+            if (crystalTwoValue === "") {
+                crystalTwoValue = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
+                console.log(crystalTwoValue);
+                userScore = parseInt(userScore + crystalTwoValue);
+                console.log("first click " + userScore);
+                $("#user-score").text(userScore);
+                scoreCompare();
+            } else {
+                userScore = parseInt(userScore + crystalTwoValue);
+                console.log("second click " + userScore);
+                $("#user-score").text(userScore);
+                scoreCompare();
+            }
+        }
+    });
 
-function scoreCompare(){
-    if(userScore > compScore) {
-        $("#result-message").text("Sorry you lose");
-        losses++;
-        $("#losses").text(losses);
+    $("#crystal-three").on("click", function () {
+        if (gameComplete === false) {
+            if (crystalThreeValue === "") {
+                crystalThreeValue = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
+                console.log(crystalThreeValue);
+                userScore = parseInt(userScore + crystalThreeValue);
+                console.log("first click " + userScore);
+                $("#user-score").text(userScore);
+                scoreCompare();
+            } else {
+                userScore = parseInt(userScore + crystalThreeValue);
+                console.log("second click " + userScore);
+                $("#user-score").text(userScore);
+                scoreCompare();
+            }
+        }
+    });
+
+    $("#crystal-four").on("click", function () {
+        if (gameComplete === false) {
+            if (crystalFourValue === "") {
+                crystalFourValue = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
+                console.log(crystalFourValue);
+                userScore = parseInt(userScore + crystalFourValue);
+                console.log("first click " + userScore);
+                $("#user-score").text(userScore);
+                scoreCompare();
+            } else {
+                userScore = parseInt(userScore + crystalFourValue);
+                console.log("second click " + userScore);
+                $("#user-score").text(userScore);
+                scoreCompare();
+            }
+        }
+    });
+
+    function scoreCompare() {
+
+        if (userScore > compScore) {
+            gameComplete = true;
+            $("#result-message").text("Sorry you lose");
+            losses++;
+            $("#losses").text(losses);
+        } else if (userScore === compScore) {
+            gameComplete = true;
+            $("#result-message").text("You WIN!");
+            wins++;
+            $("#wins").text(wins);
+        }
+    };
+
+    function reset() {
         crystalOneValue = "";
         crystalTwoValue = "";
         crystalThreeValue = "";
         crystalFourValue = "";
         userScore = "";
-            // compScore = "";
         $("#user-score").text("");
-        // $("#comp-score").text("");
-            // compScore = (Math.floor(Math.random() * (120 - 19 + 1)) + 19);
-        // $("#comp-score").text(compScore);
-    } else if (userScore === compScore) {
-        $("#result-message").text("You WIN!");
-        wins++;
-        $("#wins").text(wins);
-        crystalOneValue = "";
-        crystalTwoValue = "";
-        crystalThreeValue = "";
-        crystalFourValue = "";
-        userScore = "";
-            // compScore = "";
-        $("#user-score").text("");
-        // $("#comp-score").text("");
-            // compScore = (Math.floor(Math.random() * (120 - 19 + 1)) + 19);
-        // $("#comp-score").text(compScore);
-    } 
-};
+        compScore = (Math.floor(Math.random() * (120 - 19 + 1)) + 19);
+        $("#comp-score").text(compScore);
+        $("#result-message").text("");
+        gameComplete = false;
+    };
 
 
 
-//set a function so that on-click a random number (1-12) is generated
-    //remember to store the new number for the duration of the game
-    //add the number to the userScore
-//each time the user clicks on the object, add the new value to the userScore
-//if the userScore is === randomNumber user wins
-    //add to Wins
-    //reset game
-//else the userScore is > randomNumber user losses
-    //add to losses
-    //reset game
 
 });
 
